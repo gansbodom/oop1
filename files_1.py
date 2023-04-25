8bc14ef039a90dc09453bd00c23d11cfec2a27cd
bn <gansbodom@gmail.com>
2023-04-25 18:30:08 +0500

    Задание 2, рецепты

--- files_1.py
+++ files_1.py
@@ -9,3 +9,40 @@ def my_cook_book():
                 tmp.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
             cook_book[name] = tmp
     return cook_book
+
+
+###
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        if dish in cook_book.keys():
+            for ingredient in cook_book[dish]:
+                new_shop_list_item = dict(ingredient)
+
+                new_shop_list_item['quantity'] *= person_count
+                if new_shop_list_item['ingredient_name'] not in shop_list:
+                    shop_list[new_shop_list_item['ingredient_name']] = new_shop_list_item
+                else:
+                    shop_list[new_shop_list_item['ingredient_name']]['quantity'] += new_shop_list_item['quantity']
+        else:
+            print(f'Рецепта "{dish}" в книге нет')
+    return shop_list
+
+
+def print_shop_list(shop_list):
+    for shop_list_item in shop_list.values():
+        print('{} {} {}'.format(shop_list_item['ingredient_name'], shop_list_item['quantity'],
+                                shop_list_item['measure']))
+
+
+def create_shop_list():
+    person_count = int(input('Введите количество человек: '))
+    dishes = input('Введите блюда в расчете на одного человека (через запятую): ') \
+        .lower().split(', ')
+    shop_list = get_shop_list_by_dishes(dishes, person_count)
+    print_shop_list(shop_list)
+
+
+cook_book = my_cook_book()
+create_shop_list()
