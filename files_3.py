d1afd7eb5c0f34c22b813ca516eda64f4fe3427a
bn <gansbodom@gmail.com>
2023-04-26 17:39:33 +0500

    Задание 3

--- files_3.py
+++ files_3.py
@@ -1,5 +1,26 @@
-lst = ['./sorted/1.txt', './sorted/2.txt', './sorted/3.txt']
+import os
 
-for file in lst:
-    print(file)
-    print(sum(1 for line in open(file, 'r')))
+
+class TextFile:
+    def __init__(self, name):
+        self.name = name
+        self.row_count = sum(1 for _ in open(self.name, 'r'))
+
+
+os.chdir('./sorted')
+
+files = [TextFile('1.txt'),
+         TextFile('2.txt'),
+         TextFile('3.txt')]
+
+files.sort(key=lambda x: x.row_count)
+
+with open('all.txt', 'w') as target:
+    for i in files:
+        target.write(f'{i.name} \n')
+        target.write(f'{i.row_count} \n')
+        for line in open(i.name, 'r').readlines():
+            target.write(line)
+        target.write('\n')
+
+target.close()
