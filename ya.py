00d1dabe514dbb715e08911207fd83bba1614c79
bn <gansbodom@gmail.com>
2023-05-29 16:29:34 +0500

    Сделал задание 2 (requests)

---
+++ ya.py
@@ -0,0 +1,26 @@
+import os
+import requests
+
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def upload(self, file_path: str):
+        """Метод загружает файлы по списку file_list на яндекс диск"""
+        # Тут ваша логика
+        # Функция может ничего не возвращать
+        self.file_path = file_path
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resourses/upload'
+        headers = {'Content-Type': 'application/json', 'Autorization': f'OAuth {self.token}'}
+        params = {"path": f"{self.file_path}"}
+        r = requests.get(upload_url, headers=headers, params=params)
+        print(r.json())
+        href = r.json()["href"]
+        responce = requests.put(href, data=open(self.file_path, 'rb'))
+
+
+if __name__ == '__main__':
+    # Получить путь к загружаемому файлу и токен от пользователя
+    path_to_file = os.path.abspath('/home/bn/python/PycharmProjects/oop1/test.txt')
+    uploader = YaUploader('y0_AgAAAABR2QGiAADLWwAAAADjyvy_4jNywNthRsO5PHTsIPOSpToCxrw')
+    result = uploader.upload(path_to_file)
--- yandex_drive.py
+++ yandex_drive.py
@@ -21,26 +21,31 @@ import requests
 
 class YaUploader:
     def __init__(self, token: str):
-        self.token = 'y0_AgAAAABR2QGiAADLWwAAAADjyvy_4jNywNthRsO5PHTsIPOSpToCxrw'
+        self.token = token
 
-    def get_headers(self):
-        return {
-            'Content-Type': 'application/json',
-            'Autorization': f'OAuth {self.token}'
-        }
+    # def get_headers(self):
+    #     return {
+    #         'Content-Type': 'application/json',
+    #         'Autorization': f'OAuth {self.token}'
+    #     }
 
     def upload(self, file_path: str):
         """Метод загружает файлы по списку file_list на яндекс диск"""
-        upload_url = "https://cloud-api.yandex.net/v1/disk/resourses/upload"
-        headers = self.get_headers()
-        responce = requests.put(url="https://cloud-api.yandex.net/, data="./test.txt")
-
+        # upload_url = "https://cloud-api.yandex.net/v1/disk/resourses/upload"
+        # headers = self.get_headers()
+        # responce = requests.put(url="https://cloud-api.yandex.net/, data="./test.txt")
         # Тут ваша логика
         # Функция может ничего не возвращать
+        self.file_path = file_path
+        headers = {'Content-Type': 'application/json', 'Autorization': f'OAuth {self.token}'}
+        params = {"path": "Тест API"}
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resourses/upload"
+        href = requests.get(upload_url, headers=headers)
+        responce = requests.put(href, data=open(file_path, 'rb'))
 
 if __name__ == '__main__':
     # Получить путь к загружаемому файлу и токен от пользователя
-    path_to_file = ...
-    token = ...
+    path_to_file = 'test.txt'
+    token = 'y0_AgAAAABR2QGiAADLWwAAAADjyvy_4jNywNthRsO5PHTsIPOSpToCxrw'
     uploader = YaUploader(token)
     result = uploader.upload(path_to_file)
